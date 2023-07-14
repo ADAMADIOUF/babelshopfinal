4b30431b50a4c053531e227bef30af6910f3f381
adama diouf <adamadiouf2017@gmail.com>
2023-07-14 10:58:05 -0500

    Initial commit

---
+++ frontend/src/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    name: 'Lamoutor Vintage Perfume ',
+    image: '/images/1.jpeg',
+    description:
+      'Lamoutor Vintage Perfume Spray Bottle 100ml Pink Vintage Refillable Perfume Bottle with Long Tassel.',
+    brand: 'Brand A',
+    category: 'Category X',
+    price: 19.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 20,
+  },
+  {
+    name: 'Ariana Grande Sweet',
+    image: '/images/2.jpeg',
+    description:
+      'Ariana Grande Sweet Like Candy Eau De Parfum - 1 oz spray bottle.',
+    brand: 'Brand B',
+    category: 'Category Y',
+    price: 29.99,
+    countInStock: 5,
+    rating: 3.8,
+    numReviews: 10,
+  },
+  {
+    name: 'Miss Dior Blooming Bouquet',
+    image: '/images/3.jpeg',
+    description: `Miss Dior Blooming Bouquet: Eau de Toilette Women's Perfume | DIOR.`,
+    brand: 'Brand C',
+    category: 'Category X',
+    price: 14.99,
+    countInStock: 2,
+    rating: 4.2,
+    numReviews: 8,
+  },
+  {
+    name: 'Pride and Prejudice Perfume',
+    image: '/images/4.jpeg',
+    description: 'Pride and Prejudice Perfume — Immortal Perfumes.',
+    brand: 'Brand A',
+    category: 'Category Z',
+    price: 39.99,
+    countInStock: 0,
+    rating: 4.8,
+    numReviews: 15,
+  },
+  {
+    name: 'Tiffany & Co. Rose Gold Eau de Parfum',
+    image: '/images/5.jpeg',
+    description:
+      'Bright and effervescent, Tiffany & Co. Rose Gold fragrance embodies the spirit of optimism. Created by master perfumer Jerome Epinette of Robertet,.',
+    brand: 'Brand B',
+    category: 'Category Y',
+    price: 24.99,
+    countInStock: 7,
+    rating: 4.1,
+    numReviews: 12,
+  },
+  {
+    name: `Viktor&Rolf
+Flowerbomb`,
+    image: '/images/6.jpeg',
+    description:`'Viktor&Rolf
+Flowerbomb.`,
+    brand: 'Brand C',
+    category: 'Category Z',
+    price: 49.99,
+    countInStock: 3,
+    rating: 4.6,
+    numReviews: 18,
+  },
+]
+export default products
